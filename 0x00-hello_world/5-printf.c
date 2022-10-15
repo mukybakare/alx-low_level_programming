f2f68a4f62125d72521044706984479ea84ad33e
mukybakare <muky.bakare@yahoo.com>
2022-10-15 13:10:32 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+*main - heading of code
+*Description: print hello
+*Return: value of zero
+*/
+#include <stdio.h>
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
