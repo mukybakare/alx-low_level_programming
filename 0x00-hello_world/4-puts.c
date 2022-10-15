513c7f45edacc0f8bffc9fc54284aa0aaaf64f45
mukybakare <muky.bakare@yahoo.com>
2022-10-15 09:51:36 -0700

    Notice

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ * main - starting point
+ * Description: insert program
+ * Return: Value 0
+ */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-	puts ("\n");
-	return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
